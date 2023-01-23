9f806004d418ce70c8c8db06d27e34ba8b7d2f23
zdnMuh <2018044@scholar.itn.ac.id>
2023-01-23 19:42:59 +0700

    Query max, min, avg, dan quiz group by ... having

--- min-max-avg.sql
+++ min-max-avg.sql
@@ -42,3 +42,10 @@ select product_id, avg(pinalty) as total
 from invoice
 group by product_id
 having avg(pinalty) > 30000;
+
+-- Lakukan query untuk dapat mengeluarkan product_id, rata-rata nilai pinalty dan jumlah customer_id yang di group by berdasarkan product_id
+-- yang memiliki jumlah customer_id diatas nilai 20.
+select product_id, avg(pinalty), count(customer_id) as total
+from invoice
+group by product_id
+having count(customer_id) > 20;
