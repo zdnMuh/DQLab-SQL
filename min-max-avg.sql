ac753c5da0e2d4507ec7c2bbd38af7d0ee2d9bdd
zdnMuh <2018044@scholar.itn.ac.id>
2023-01-23 19:19:12 +0700

    Query max, min, avg pada group by ... having

---
+++ min-max-avg.sql
@@ -0,0 +1,44 @@
+-- tanpa having
+select product_id, max(total_price) as total
+from invoice
+group by product_id;
+-- dengan having
+select product_id, max(total_price) as total
+from invoice
+group by product_id
+having max(total_price) > 1000000;
+-- dengan having
+select product_id, max(pinalty) as total
+from invoice
+group by product_id
+having max(pinalty) > 30000;
+
+-- tanpa having
+select product_id, min(total_price) as total
+from invoice
+group by product_id;
+-- dengan having
+select product_id, min(total_price) as total
+from invoice
+group by product_id
+having min(total_price) < 500000;
+-- dengan having
+select product_id, min(pinalty) as total
+from invoice
+group by product_id
+having min(pinalty) < 50000;
+
+-- tanpa having
+select product_id, avg(total_price) as total
+from invoice
+group by product_id;
+-- dengan having
+select product_id, avg(total_price) as total
+from invoice
+group by product_id
+having avg(total_price) > 100000;
+-- dengan having
+select product_id, avg(pinalty) as total
+from invoice
+group by product_id
+having avg(pinalty) > 30000;
