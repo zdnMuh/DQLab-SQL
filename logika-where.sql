a704e3651cfd151dd99f136758a7bb7075e6038c
zdnMuh <2018044@scholar.itn.ac.id>
2023-01-25 10:11:09 +0700

    Query logika-where

--- logika-where.sql
+++ logika-where.sql
@@ -1,2 +1,6 @@
 select distinct * from data_retail
 where Product = 'Jaket' AND Average_Transaction_Amount >= 1000000;
+
+select distinct Customer_ID
+from summary_transaction
+where Average_transaction < 1000000 and product = 'Sepatu';
