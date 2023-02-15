91bccf05a2a52df84688af9ebfa3d9844dc3d65c
zdnMuh <2018044@scholar.itn.ac.id>
2023-02-15 12:06:10 +0800

    Query mengolah teks

--- mengolahTeks.sql
+++ mengolahTeks.sql
@@ -3,3 +3,10 @@ as kotaLahir
 from dqlabdatateks;
 
 -- Agung|||Semarang|||03 Juni 1992|||Jawa Tengah
+
+-- 1
+SELECT SUBSTR(isi, LOCATE('|||', isi) + 3, LOCATE('|||', isi, LOCATE('|||', isi) + 3) - LOCATE('|||', isi) - 3) AS tanggalLahir
+FROM tabel;
+-- 2
+SELECT SUBSTR(isi, LOCATE('|||', isi, LOCATE('|||', isi) + 1) + 3, LOCATE('|||', isi, LOCATE('|||', isi, LOCATE('|||', isi) + 1) + 1) - LOCATE('|||', isi, LOCATE('|||', isi) + 1) - 3) as TanggalLahir
+FROM dqlabdatateks;
